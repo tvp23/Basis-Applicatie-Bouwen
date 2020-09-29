b93359bfe10c6916517672e5d690f937923e0bb3
tvp23 <70744506+tvp23@users.noreply.github.com>
2020-09-29 16:12:41 +0200

    BAB2
    Klaar

---
+++ B.A.B 2/includes/Head.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Mijn website</title>
+		<meta name="description" content="Mijn website met mijn projecten(bab).">
+		<meta name="author" content="Twan van Paridon">
+		<meta name="viewport" content="width=device-width">
+	
+		<link rel="stylesheet" href="style.css" type="text/css" >
+		
+
+	</head>
---
+++ B.A.B 2/index.php
@@ -0,0 +1,18 @@
+<?php
+include "includes/Head.php"
+?>
+
+<body>
+	
+	<div class = "form">
+		<form action="FormResult.php">
+			<label class="Signup" type="text" >Sign Up</label><br>
+			<input class="Username" type="text" placeholder="Username"><br>
+			<input class="Password" type="password" placeholder="Password"><br>
+			<button class ="submit" type="submit" value="Submit">  <img class="lock" src="https://library.kissclipart.com/20180829/pcw/kissclipart-lock-icon-png-blue-clipart-blue-clip-art-cd1b742fe76a31a0.png" alt="Blue Lock" width=20px; height=25px;> <p>Log-In</p>
+		</form>
+	</div>
+	<h4>Darkmode Neomorphic</h4>
+
+</body>
+</html>
