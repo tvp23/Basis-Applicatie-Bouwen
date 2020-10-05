ca8f788f4c91c8e52b24b1a625b9caa904afbcbe
tvp23 <70744506+tvp23@users.noreply.github.com>
2020-10-05 14:50:13 +0200

    bab 3

---
+++ B.A.B 3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css" >
+    <title>BAB3</title>
+</head>
+
+<body>
+
+    <form class=flexform id=flexform action="">
+        <input class=flex_item id=Name type="text">
+        <input class=flex_item id=LastName type="text">
+        <input class=flex_item id=Age type="text">
+        <input class=flex_item id=Place type="text">
+        <input class=flex_item id=Submit type="submit">
+    </form>
+
+</body>
